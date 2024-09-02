f3d448cb794e235968aacc1c0a0fd34373c252fc
muhamadpadlan <padlanmarhas@gmail.com>
2024-09-02 12:24:38 +0700

    commit pertama

---
+++ latihan_1/lib/Beranda.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Beranda extends StatelessWidget {
+  const Beranda({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [Text('Hello World')],
+      ),
+    );
+  }
+}
